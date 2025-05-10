de0c67e4d5b5e6407a2d3bb002145e6a2ff346f5
kimkimjunjun <wnsgnl323@gmail.com>
2025-05-10 14:57:43 +0900

    포트폴리오 시작

---
+++ components/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function SideBar() {
+    return (
+        <div className="fixed left-0 h-screen w-[10rem] border-r border-black">
+            <span></span>
+        </div>
+    )
+}
---
+++ components/typing/typing.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useState } from "react";
+
+interface TimeProps {
+    onTypingComplete: () => void; // 타이핑이 완료되었을 때 호출할 콜백 함수
+}
+
+export default function TypingComponent({ onTypingComplete }: TimeProps) {
+    const [word, setWord] = useState('');
+    let sentence = "안녕하세요\n 프론트엔드 개발자 김준휘입니다.";
+    const currentIndex = useRef(0);
+
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            if (sentence.length > currentIndex.current) {
+                // 직접 currentIndex.current 값을 사용하여 현재 문자를 가져오고 업데이트
+                const nextChar = sentence[currentIndex.current];
+                setWord((word) => word + nextChar);
+                currentIndex.current += 1;
+            } else {
+                clearInterval(timerId);
+                onTypingComplete(); // 모든 글자가 나왔을 때 부모 컴포넌트에 알림
+            }
+        }, 60);
+
+        return () => clearInterval(timerId);
+    }, [onTypingComplete]);
+
+    const renderedText = word.split('\n').map((line, index) => (
+        <React.Fragment key={index}>
+            {line}
+            {index < word.split('\n').length - 1 && <br />}
+        </React.Fragment>
+    ));
+
+    return (
+        <>
+            <p className="text-white text-xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-semibold m-auto text-center flex">{renderedText}</p>
+        </>
+    );
+};
