595a253005114ccf0419b666befada92c65a84bd
kimkimjunjun <wnsgnl323@gmail.com>
2025-05-14 21:24:38 +0900

    피드백버튼 수정중

--- components/upbutton.tsx
+++ components/upbutton.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import React, { useState, useEffect, useCallback } from 'react';
+import feedback from "@/public/images/feedback.png"
 
 export default function Upbutton() {
     const [isVisible, setIsVisible] = useState(false);
@@ -39,22 +41,42 @@ export default function Upbutton() {
     };
 
     return (
-        <div className={`
-            fixed right-5 bottom-5 z-20 group flex items-center transition-opacity duration-300 ease-in-out 
-            ${isVisible ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} 
+        <div>
+            <div className={`
+            fixed right-5 z-20 group flex items-center transition-[bottom] duration-300 ease-in-out 
+            ${isVisible ? 'bottom-5 pointer-events-auto' : 'bottom-[-5rem] pointer-events-none'} 
         `}>
-            <span className="
+                <span className="
                 text-white font-semibold text-sm mr-2 bg-gray-600 p-[0.5rem] rounded-[0.8rem] opacity-0 transition-opacity duration-300 group-hover:opacity-100
             ">
-                위로가기
-            </span>
+                    위로가기
+                </span>
 
-            <button
-                className="hover:opacity-80 w-fit shadow-lg p-[1rem] rounded-full bg-amber-100 cursor-pointer z-10"
-                onClick={handleScrollToTop}
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 28 17" fill="none"><path d="M2 15L14 3L26 15" stroke="black" stroke-width="3" stroke-linecap="round"></path></svg>
-            </button>
+                <button
+                    className="hover:opacity-80 w-fit shadow-lg p-[1rem] rounded-full bg-amber-100 cursor-pointer z-10"
+                    onClick={handleScrollToTop}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-arrow-up-to-line"><path d="M5 3h14"></path><path d="m18 13-6-6-6 6"></path><path d="M12 7v14"></path></svg>
+                </button>
+            </div>
+            {/* <div className={`
+            fixed right-5 z-20 group flex items-center transition-[bottom] duration-300 ease-in-out pointer-events-auto
+            ${isVisible ? 'bottom-22' : 'bottom-5'}
+        `}>
+                <span className="
+                text-white font-semibold text-sm mr-2 bg-gray-600 p-[0.5rem] rounded-[0.8rem] opacity-0 transition-opacity duration-300 group-hover:opacity-100
+            ">
+                    피드백주기
+                </span>
+
+                <button
+                    className="hover:opacity-80 w-fit shadow-lg p-[1rem] rounded-full bg-amber-100 cursor-pointer z-10"
+                >
+                    <Image src={feedback} width={20} height={20} alt='' />
+                </button>
+            </div> */}
+
+
         </div>
     )
 }
