67bbc5293071163bc471b263e3360d7b1593cd55
kimkimjunjun <wnsgnl323@gmail.com>
2025-05-14 18:03:54 +0900

    오타수정

--- components/ground/education.tsx
+++ components/ground/education.tsx
@@ -9,7 +9,7 @@ export default function Education() {
                 </div>
                 <div className="flex flex-col">
                     <span className="text-[1rem] lg:text-[1.5rem]">소프트웨어융합대학 컴퓨터학부</span>
-                    <span className="text-[1rem] lg:text-[1.5rem] text-gray-500">졸업 예정정</span>
+                    <span className="text-[1rem] lg:text-[1.5rem] text-gray-500">졸업 예정</span>
                 </div>
                 <div className="2xl:w-[70rem] xl:w-[61rem] flex items-center mt-[2rem]">
                     <span className="text-[1rem] lg:text-[2rem] font-bold">KYUNGHEE HIGH SCHOOL</span>
