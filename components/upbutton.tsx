18873f455f42c9d4b5bcad64ec5add7daec3c55b
kimkimjunjun <wnsgnl323@gmail.com>
2025-05-14 22:08:59 +0900

    svg파일 옵션이름 수정

--- components/upbutton.tsx
+++ components/upbutton.tsx
@@ -56,7 +56,7 @@ export default function Upbutton() {
                     className="hover:opacity-80 w-fit shadow-lg p-[1rem] rounded-full bg-amber-100 cursor-pointer z-10"
                     onClick={handleScrollToTop}
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-arrow-up-to-line"><path d="M5 3h14"></path><path d="m18 13-6-6-6 6"></path><path d="M12 7v14"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 3h14"></path><path d="m18 13-6-6-6 6"></path><path d="M12 7v14"></path></svg>
                 </button>
             </div>
             {/* <div className={`
