c9ea7566085a95ea9f4534b897b061a886edabd5
kimkimjunjun <wnsgnl323@gmail.com>
2025-05-28 17:40:23 +0900

    사이트 제목 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <title>개발자 김준휘 포트폴리오</title>
+        <meta name="description" content="My PORTFOLIO" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
