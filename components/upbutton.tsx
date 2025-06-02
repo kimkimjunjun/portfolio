e8d195a878e0a29892ad01df0e1e169c78157e07
kimkimjunjun <wnsgnl323@gmail.com>
2025-06-02 17:46:51 +0900

    이름밑줄 애니메이션

--- components/upbutton.tsx
+++ components/upbutton.tsx
@@ -59,23 +59,6 @@ export default function Upbutton() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 3h14"></path><path d="m18 13-6-6-6 6"></path><path d="M12 7v14"></path></svg>
                 </button>
             </div>
-            {/* <div className={`
-            fixed right-5 z-20 group flex items-center transition-[bottom] duration-300 ease-in-out pointer-events-auto
-            ${isVisible ? 'bottom-22' : 'bottom-5'}
-        `}>
-                <span className="
-                text-white font-semibold text-sm mr-2 bg-gray-600 p-[0.5rem] rounded-[0.8rem] opacity-0 transition-opacity duration-300 group-hover:opacity-100
-            ">
-                    피드백주기
-                </span>
-
-                <button
-                    className="hover:opacity-80 w-fit shadow-lg p-[1rem] rounded-full bg-amber-100 cursor-pointer z-10"
-                >
-                    <Image src={feedback} width={20} height={20} alt='' />
-                </button>
-            </div> */}
-
         </div>
     )
 }
